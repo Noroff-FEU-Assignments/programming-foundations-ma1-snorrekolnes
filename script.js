83b2a5bf70b9c0f2c3389272054387799bac4e22
snorre kolnes røgenes <snorre.kolnes@hotmail.com>
2022-01-09 19:17:39 +0100

    add
    added js and html

---
+++ script.js
@@ -0,0 +1,19 @@
+var string = "hello";
+var number = 520;
+var boolean = (2+2===4);
+console.log (string);
+console.log (number);
+console.log (boolean);
+var fName = "snorre";
+var sName = "kolnes";
+var fullname = fName + sName;
+console.log(fullname);
+var pet ="frog";
+typeof(pet);
+var ifShipped = "true"
+console.log (ifShipped)
+var notShipped ="false"
+console.log (notShipped)
+for (var count= 7;count<=13;count++) {
+    console.log (count);
+}
